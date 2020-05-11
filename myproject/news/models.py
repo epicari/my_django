c352255e2ae16d83d50f0950a89301b1c43ffa79
epicari <aiek261247@gmail.com>
2020-05-11 16:29:47 +0900

    test

--- myproject/news/models.py
+++ myproject/news/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
 # Create your models here.
+# 이 클래스는 추후 DB의 Table로 사용함
+# titles와 link 라는 column을 가진 News라는 이름의 Table 
 
 class News(models.Model):
-    news_id = models.AutoField(primary_key=True)
-    news_heading = models.CharField(max_length=250)
-    news_body = models.TextField()
+    news_titles = models.CharField(max_length=255)
+    news_link = models.URLField()
+
+    def __str__(self):
+        return self.news_titles
---
+++ myproject/web_crw_news_titles.py
@@ -0,0 +1,29 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+import django
+django.setup()
+
+from news.models import News
+
+import requests
+from bs4 import BeautifulSoup
+
+def news_titles():
+    url = 'https://news.daum.net/breakingnews/digital'
+    r = requests.get(url)
+    html = r.content
+
+    soup = BeautifulSoup(html, 'html.parser')
+    titles = soup.select('div.cont_thumb > strong > a')
+
+    data = {}
+
+    for title in titles:
+        data[title.text] = title.get('href')
+    
+    return data
+
+if __name__ == "__main__":
+    news_dict = news_titles()
+    for t, l in news_dict.items():
+        News(news_titles=t, news_link=l).save()
