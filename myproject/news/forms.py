6bc2923e09e6a7cecf1bfda73da81d6536601826
epicari <aiek261247@gmail.com>
2020-06-22 19:34:25 +0900

    del naver news, factory method crw.py

--- myproject/news/forms.py
+++ myproject/news/forms.py
@@ -1,12 +1,7 @@
 from django import forms
-from .models import N_news, D_news
+from .models import D_news
 
 class DnewsForm(forms.ModelForm):
     class Meta:
         model = D_news
         fields = ["dnews_titles", "dnews_links"]
-
-class NnewsForm(forms.ModelForm):
-    class Meta:
-        model = N_news
-        fields = ["nnews_titles", "nnews_links"]
--- myproject/news/models.py
+++ myproject/news/models.py
@@ -1,19 +1,8 @@
 from django.db import models
 
-# Create your models here.
-# 이 클래스는 추후 DB의 Table로 사용함
-# titles와 link 라는 column을 가진 News라는 이름의 Table 
-
 class D_news(models.Model):
     dnews_titles = models.CharField(max_length=255)
     dnews_links = models.URLField()
 
     def __str__(self):
         return self.dnews_titles
-
-class N_news(models.Model):
-    nnews_titles = models.CharField(max_length=255)
-    nnews_links = models.URLField()
-
-    def __str__(self):
-        return self.nnews_titles
